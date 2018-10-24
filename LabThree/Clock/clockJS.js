9b14fe8b050afc5591563a74288845497918ce80
Katie Obrien <katie-obrien06@hotmail.com>
2018-10-24 12:10:42 +0100

    Lab Three added

---
+++ LabThree/Clock/clockJS.js
@@ -0,0 +1,15 @@
+let canvasLength = 850;
+let radius = canvasLength/2;
+
+let canvas = document.getElementById("canvas");
+canvas.height = canvasLength;
+canvas.width = canvasLength;
+
+function drawClock(){
+
+}
+
+function drawHand(){
+
+}
+window.onload = setInterval(drawClock, 1000);
